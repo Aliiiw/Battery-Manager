5e4c7564f69b18fc1d3f1c67bb61da522660c9b7
Ali Rahimi <1aliirahimi1@gmail.com>
2022-07-24 16:06:47 +0430

    create project

---
+++ app/src/main/java/com/alirahimi/batterymanager/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.alirahimi.batterymanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
