f10bf531594e9cfd1ab57fee70a6600c8a06f079
Ali Rahimi <1aliirahimi1@gmail.com>
2022-07-24 20:04:17 +0430

    get the fields from battery

--- app/src/main/java/com/alirahimi/batterymanager/MainActivity.kt
+++ app/src/main/java/com/alirahimi/batterymanager/MainActivity.kt
@@ -1,13 +1,34 @@
 package com.alirahimi.batterymanager
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        registerReceiver(batteryDataReceiver, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
 
+    }
+
+    private var batteryDataReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            var level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0)
+            var batteryPlugged = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, 0)
+            var batteryTemp = intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, 0) / 10
+            var batteryVoltage = intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE, 0) / 1000
+            Log.e("3636", level.toString())
+            Log.e("3636", batteryPlugged.toString())
+            Log.e("3636", batteryTemp.toString())
+            Log.e("3636", batteryVoltage.toString())
+
+        }
     }
 }
--- app/src/main/java/com/alirahimi/batterymanager/SplashActivity.kt
+++ app/src/main/java/com/alirahimi/batterymanager/SplashActivity.kt
@@ -3,7 +3,6 @@ package com.alirahimi.batterymanager
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import com.alirahimi.batterymanager.databinding.ActivitySplashBinding
 import java.util.*
 import kotlin.concurrent.timerTask
@@ -12,20 +11,17 @@ class SplashActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySplashBinding
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
-
         val quotes = arrayOf("What are you looking for?", "Almost Done!", "Let's Go")
 
         for (i in 1..3) {
             textGenerator((i * 1000).toLong(), quotes[i - 1])
         }
-
 
         Timer().schedule(timerTask {
             startActivity(Intent(this@SplashActivity, MainActivity::class.java))
