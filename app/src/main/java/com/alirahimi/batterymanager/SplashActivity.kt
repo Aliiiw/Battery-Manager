c8a899d6151ce3abe0a6dedb2572585a4a521c8d
Ali Rahimi <1aliirahimi1@gmail.com>
2022-07-24 19:34:55 +0430

    create quotes for loading splash

--- app/src/main/java/com/alirahimi/batterymanager/SplashActivity.kt
+++ app/src/main/java/com/alirahimi/batterymanager/SplashActivity.kt
@@ -3,18 +3,38 @@ package com.alirahimi.batterymanager
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import java.util.*
 import kotlin.concurrent.timerTask
 
 class SplashActivity : AppCompatActivity() {
+    lateinit var tempText: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        tempText = findViewById(R.id.splash_text)
+
+        val quotes = arrayOf("What are you looking for?", "Almost Done!", "Let's Go")
+
+        for (i in 1..3) {
+            textGenerator((i * 1000).toLong(), quotes[i - 1])
+        }
+
+
         Timer().schedule(timerTask {
             startActivity(Intent(this@SplashActivity, MainActivity::class.java))
             finish()
-        }, 2050)
+        }, 4000)
 
+    }
+
+    private fun textGenerator(delayTime: Long, textToShow: String) {
+        Timer().schedule(timerTask {
+            runOnUiThread(timerTask {
+                tempText.text = textToShow
+            })
+        }, delayTime)
     }
 }
