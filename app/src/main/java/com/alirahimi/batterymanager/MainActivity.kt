61ce0a3d429ec64021d1019afd8e5aec0fecd4ba
Ali Rahimi <1aliirahimi1@gmail.com>
2022-07-26 11:37:39 +0430

    get the data from binding

--- app/src/main/java/com/alirahimi/batterymanager/MainActivity.kt
+++ app/src/main/java/com/alirahimi/batterymanager/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.alirahimi.batterymanager
 
+import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -8,27 +9,38 @@ import android.os.BatteryManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.alirahimi.batterymanager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         registerReceiver(batteryDataReceiver, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
 
     }
 
     private var batteryDataReceiver: BroadcastReceiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context, intent: Intent) {
-            var level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0)
-            var batteryPlugged = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, 0)
-            var batteryTemp = intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, 0) / 10
-            var batteryVoltage = intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE, 0) / 1000
-            Log.e("3636", level.toString())
-            Log.e("3636", batteryPlugged.toString())
-            Log.e("3636", batteryTemp.toString())
-            Log.e("3636", batteryVoltage.toString())
 
+        override fun onReceive(context: Context, intent: Intent) {
+
+            binding.textTemperature.text =
+                (intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, 0) / 10).toString() + " °C"
+            binding.textVoltage.text =
+                (intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE, 0) / 1000).toString() + " volt"
+
+            binding.textCpu.text = intent.getStringExtra(BatteryManager.EXTRA_TECHNOLOGY)
+            if (intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, 0) == 0) {
+                binding.textPlug.text = "Not-Plugged"
+            } else {
+                binding.textPlug.text = "Plugged-In"
+            }
         }
     }
 }
